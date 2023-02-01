f88c7be98b3a40a014b371676e8dac8ef88cd012
rodric rabbah <rodric@gmail.com>
2023-01-31 19:47:47 -0500

    skip pip in venv for packaging function
    run with `LOCAL_DEV=* build.sh` locally to activate the venv otherwise do an optimized pip install for remote function builds.

--- functions/packages/simaster/progress_kkn/build.sh
+++ functions/packages/simaster/progress_kkn/build.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-virtualenv virtualenv
-source virtualenv/bin/activate
-pip install -r requirements.txt
-deactivate
+# do this for local development, but for packaging the function, skip pip in the venv
+if [ -n "${LOCAL_DEV}" ]; then
+  virtualenv virtualenv
+  source virtualenv/bin/activate
+  pip install -r requirements.txt
+  deactivate
+else
+  virtualenv --without-pip virtualenv
+  pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
+fi
